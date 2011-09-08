08c48c91cdf9935359a56761eb8c9fef44e63df0
Kristian Jacobsen <kracobsen@gmail.com>
2011-09-08 16:38:19 +0200

    Endring i Token.java

--- no/uio/ifi/cless/scanner/Scanner.java
+++ no/uio/ifi/cless/scanner/Scanner.java
@@ -7,6 +7,7 @@ package no.uio.ifi.cless.scanner;
 import no.uio.ifi.cless.chargenerator.CharGenerator;
 import no.uio.ifi.cless.error.Error;
 import no.uio.ifi.cless.log.Log;
+
 import static no.uio.ifi.cless.scanner.Token.*;
 
 /*
@@ -17,67 +18,73 @@ public class Scanner {
     public static String curName, nextName, nextNextName;
     public static int curNum, nextNum, nextNextNum;
     public static int curLine, nextLine, nextNextLine;
-	
+
     public static void init() {
-	//TODO
+        //TODO
     }
-	
+
     public static void finish() {
-	//TODO
+        //TODO
     }
-	
+
     public static void readNext() {
-	curToken = nextToken;  nextToken = nextNextToken;
-	curName = nextName;  nextName = nextNextName;
-	curNum = nextNum;  nextNum = nextNextNum;
-	curLine = nextLine;  nextLine = nextNextLine;
+        curToken = nextToken;
+        nextToken = nextNextToken;
+        curName = nextName;
+        nextName = nextNextName;
+        curNum = nextNum;
+        nextNum = nextNextNum;
+        curLine = nextLine;
+        nextLine = nextNextLine;
 
-	nextNextToken = null;
-	while (nextNextToken == null) {
-	    nextNextLine = CharGenerator.curLineNum();
+        nextNextToken = null;
+        while (nextNextToken == null) {
+            nextNextLine = CharGenerator.curLineNum();
 
-	    if (! CharGenerator.isMoreToRead()) {
-		nextNextToken = eofToken;
-	    //TODO
-	    } else {
-		illegal("Illegal symbol: '" + CharGenerator.curC + "'!");
-	    }
-	}
-	Log.noteToken();
+            if (!CharGenerator.isMoreToRead()) {
+                nextNextToken = eofToken;
+                //TODO
+            } else {
+                illegal("Illegal symbol: '" + CharGenerator.curC + "'!");
+            }
+        }
+        Log.noteToken();
     }
-	
+
     private static boolean isLetterAZ(char c) {
-	//TODO
-	return false;
+        //TODO
+        return false;
     }
 
     // Various error reporting methods
     // (They are placed in this package because most of them include 
     // information found here.)
-	
+
     public static void illegal(String message) {
-	Error.error(curLine, message);
+        Error.error(curLine, message);
     }
 
     public static void expected(String exp) {
-	illegal(exp + " expected, but found a " + curToken + "!");
+        illegal(exp + " expected, but found a " + curToken + "!");
     }
-	
+
     public static void check(Token t) {
-	if (curToken != t)
-	    expected("A " + t);
+        if (curToken != t)
+            expected("A " + t);
     }
-	
+
     public static void check(Token t1, Token t2) {
-	if (curToken != t1 && curToken != t2)
-	    expected("A " + t1 + " or a " + t2);
+        if (curToken != t1 && curToken != t2)
+            expected("A " + t1 + " or a " + t2);
     }
-	
+
     public static void skip(Token t) {
-	check(t);  readNext();
+        check(t);
+        readNext();
     }
-	
+
     public static void skip(Token t1, Token t2) {
-	check(t1,t2);  readNext();
+        check(t1, t2);
+        readNext();
     }
 }
--- no/uio/ifi/cless/scanner/Token.java
+++ no/uio/ifi/cless/scanner/Token.java
@@ -7,19 +7,20 @@ package no.uio.ifi.cless.scanner;
 /*
  * The different kinds of tokens read by Scanner.
  */
-public enum Token { addToken, assignToken, commaToken, divideToken, elseToken,
-    eofToken, equalToken, forToken, greaterEqualToken, greaterToken, 
-    ifToken, intToken, leftBracketToken, leftCurlToken, leftParToken, 
+public enum Token {
+    addToken, assignToken, commaToken, divideToken, elseToken,
+    eofToken, equalToken, forToken, greaterEqualToken, greaterToken,
+    ifToken, intToken, leftBracketToken, leftCurlToken, leftParToken,
     lessEqualToken, lessToken, multiplyToken, nameToken, notEqualToken,
     numberToken, rightBracketToken, rightCurlToken, rightParToken,
     returnToken, semicolonToken, subtractToken, whileToken;
 
     public static boolean isOperand(Token t) {
-	return t==numberToken || t==nameToken || t==leftParToken;
+        return t == numberToken || t == nameToken || t == leftParToken;
     }
 
     public static boolean isOperator(Token t) {
-	//-- Must be changed in part 0:
-	return false;
+        //Assuming everything else counts as an operator
+        return !(t == numberToken || t == nameToken || t == leftParToken);
     }
 }
